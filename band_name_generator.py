be489d49dc11542c2fd597131343ef4dfb57ac68
temitope okelola <95000123+teesmart39@users.noreply.github.com>
2022-09-29 18:55:25 +0100

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+#Create a greeting
+print("Welcome to band name generator")
+#Ask user for the name of the city they grew up in
+city = input("What city did you grow up in?\n")
+#Ask user the name of their pet
+pet = input("What is the name of your pet?\n")
+#Combine the pet and city names
+band_name = city + "  " + pet
+print("Your band name could be " + band_name)
